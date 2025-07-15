5abe20e110091c2f704bf665579402d980bde47e
Kuvarji Gupta <139498248+kuvarji@users.noreply.github.com>
2025-07-15 10:06:20 +0530

    hhh

--- portfolio/src/Components/Complete.jsx
+++ portfolio/src/Components/Complete.jsx
@@ -5,7 +5,7 @@ function Complete(props) {
   return (
     <div className="flex w-full justify-center  mt-6">
       <div className="  sm:w-full sm:flex sm:justify-end leading-none pr-2 scroll-smooth">
-        <div className="w-[250px] sm:w-[500px] sm:h-[300px] h-[150px] border-2 rounded-md px-1 py-2 bg-slate-200">
+        <div className="w-[250px] sm:w-[500px] sm:h-[300px] h-[160px] border-2 rounded-md px-1 py-2 bg-slate-200">
           <Link to={props.link}>
           <div className="w-full h-[120px] sm:h-[220px] rounded-md bg-gradient-to-r from-slate-300 to-slate-500">
             <img
